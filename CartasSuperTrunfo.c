054fa884475a1cf097f33bcc0a24aaa461680440
DiJr10 <dinicejunior@gmail.com>
2025-08-11 03:30:40 +0000

    Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,6 +10,7 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
+	char estado1, estado2;
     char codigo1[4], codigo2[4]; // Necessário tamanho 4 para caber os 3 caracteres mais o caracter nulo \0 sem causar erro na execução.
 	char nome1[30], nome2[30];
 	int populacao1, populacao2;
@@ -19,21 +20,16 @@ int main() {
 	float densidade1, densidade2;
 	float percapita1, percapita2;
 	
-	populacao1=(float)populacao1;
-	populacao2=(float)populacao2;
-
-	densidade1=populacao1/area1;
-	densidade2=populacao2/area2;
-	percapita1=pib1/populacao1;
-	percapita2=pib2/populacao2;
-    
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
   	
     printf("Cadastro da Carta 01 - Digite os dados para esta carta e Pressione ENTER.\n\n");
 
-	printf("OBS: O código da carta é composto por 3 caracteres exemplo A02, onde a letra de A até H\n");
+	printf("Informe a letra entre A e H que representa o Estado: ");
+	scanf(" %c", &estado1); // Espaço para evitar erro e receber um Enter como resposta a ser armazenada.
+
+	printf("\n\nOBS: O código da carta é composto por 3 caracteres exemplo A02, onde a letra de A até H\n");
 	printf("     significa um Estado e o número de 01 a 04 significa uma cidade deste Estado\n\n");
 
 	printf("Informe o Código da Carta com 3 caracteres: ");
@@ -56,7 +52,10 @@ int main() {
     
     printf("\nCadastro da Carta 02 - Digite os dados para esta carta e Pressione ENTER.\n\n");
 
-	printf("OBS: O código da carta é composto por 3 caracteres exemplo E04, onde a letra de A até H\n");
+	printf("Informe a letra entre A e H que representa o Estado: ");
+	scanf(" %c", &estado2); // Espaço para evitar erro e receber um Enter como resposta a ser armazenada.
+
+	printf("\n\nOBS: O código da carta é composto por 3 caracteres exemplo E04, onde a letra de A até H\n");
 	printf("     significa um Estado e o número de 01 a 04 significa uma cidade deste Estado\n\n");
 
 	printf("Informe o Código da Carta com 3 caracteres: ");
@@ -81,27 +80,35 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+	densidade1 = (float)populacao1 / area1;
+    densidade2 = (float)populacao2 / area2;
+    percapita1 = pib1 / populacao1;
+    percapita2 = pib2 / populacao2;
+
+
 	printf("\n\nExibindo as informações das Cartas Cadastradas\n");
 
 	printf("\nInformações da Carta 01\n");
+	printf("Estado:    %c\n", estado1);
 	printf("Código:    %s\n", codigo1);
 	printf("Cidade:    %s\n", nome1);
 	printf("População: %d habitantes\n", populacao1);
-	printf("Área:      %.2f km2\n", area1);
+	printf("Área:      %.2f km²\n", area1);
 	printf("PIB:       %.2f Reais\n", pib1);
 	printf("Pontos Turísticos:      %d Locais\n", pontos1);
 	printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
 	printf("PIB per Capita:         %.2f Reais\n", percapita1);
 
 	printf("\nInformações da Carta 02\n");
+	printf("Estado:    %c\n", estado2);
 	printf("Código:    %s\n", codigo2);
 	printf("Cidade:    %s\n", nome2);
 	printf("População: %d habitantes\n", populacao2);
-	printf("Área:      %.2f km2\n", area2);
+	printf("Área:      %.2f km²\n", area2);
 	printf("PIB:       %.2f Reais\n", pib2);
 	printf("Pontos Turísticos:      %d Locais\n", pontos2);
 	printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
-	printf("PIB per Capita:         %.2f Reais\n", percapita2);
+	printf("PIB per Capita:         %.2f Reais\n\n\n", percapita2);
 
     return 0;
 }
